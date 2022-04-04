1914da9faa28d891fad5cd00175fd60bf97cc805
Fabo011 <email@threesixty-webdevelopers.com>
2022-04-04 16:41:37 +0200

    04.04.22 Fabian

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import 'dotenv/config';
 
+const MongoDB:string= process.env.MONGODB;
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(MongoDB)],
   controllers: [AppController],
   providers: [AppService],
+ 
 })
 export class AppModule {}
---
+++ src/schemas/users.schema.ts
@@ -0,0 +1,23 @@
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop()
+  username: string;
+
+  @Prop()
+  firstname: string;
+
+  @Prop()
+  lastname: string;
+
+  @Prop({unique: true})
+  email: string;
+
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
